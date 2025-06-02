1a60205ea39319386bb6216fb9f826820a6f3d6b
nathansom <nathan.sa.official@gmail.com>
2025-06-01 21:52:20 -0700

    refactor: Remove on submit

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import Script from "next/script";
-import { FormEvent, useState } from "react";
-import { toast } from "react-toastify";
+import { useState } from "react";
 
 export const Contact = ({ data }: any) => {
   const [name, setName] = useState(""),
@@ -10,48 +9,6 @@ export const Contact = ({ data }: any) => {
     [subject, setSubject] = useState(""),
     [message, setMessage] = useState(""),
     contactMessage = data?.contactmessage || "";
-
-  const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    const formData = new FormData(e.currentTarget);
-
-    try {
-      const res = await fetch("/__contact", {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: new URLSearchParams(
-          formData as unknown as Record<string, string>
-        ).toString(),
-      });
-
-      if (res.redirected) {
-        toast("Your submission is not successful. Please try again.", {
-          type: "error",
-        });
-      } else if (res.ok) {
-        toast("Your message was sent successfully. Thank you!", {
-          type: "success",
-        });
-        setName("");
-        setEmail("");
-        setSubject("");
-        setMessage("");
-      }
-    } catch (e: unknown) {
-      const errorMsg =
-        typeof e === "object" &&
-        e &&
-        "message" in e &&
-        typeof e.message === "string"
-          ? e.message
-          : typeof e === "object"
-          ? JSON.stringify(e)
-          : "Unknown error has occured";
-
-      toast(errorMsg, { type: "error" });
-    }
-  };
 
   return (
     <>
@@ -87,7 +44,6 @@ export const Contact = ({ data }: any) => {
             <form
               name="contact"
               method="POST"
-              onSubmit={onSubmit}
               data-netlify-recaptcha="true"
               data-netlify="true"
             >
